8326604e17fea8fa84d0459b2c5adb275e91bf7e
Simen Dymbe <simen.dymbe@gmail.com>
2022-10-13 16:30:02 +0200

    Add ShadowingModel class

--- slow_fading.py
+++ slow_fading.py
@@ -1,9 +1,11 @@
 from abc import ABC, abstractmethod
 import numpy as np
+from scipy.special import erf, erfc, erfinv, erfcinv
 
 class ShadowingModel:
-    def __init__(self, shadowing_std, **kwargs):
-        self.shadowing_std = shadowing_std
+    def __init__(self, shadowing_std_db, **kwargs):
+        self.shadowing_std_db = shadowing_std_db
+        self.__dict__.update(kwargs)
 
-    def prob_threshhold_signal(self):
-        pass #yet to me implemented
+    def prob_threshhold_signal(self, P_r_dbm, P_s_dbm):
+        return 1/2 + 1/2 * erf((P_r_dbm - P_s_dbm)/np.sqrt(2) * self.shadowing_std_db)
